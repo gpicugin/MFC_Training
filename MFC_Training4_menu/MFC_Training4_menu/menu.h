042e5901c9d00244947ff2888f048323cc5b9535
Unknown <gpicugin@gmail.com>
2018-11-22 13:20:17 +0300

    MFC_Training4_menu begin

---
+++ MFC_Training4_menu/MFC_Training4_menu/menu.h
@@ -0,0 +1,23 @@
+#include <afxwin.h>
+class CMainWin: public CFrameWnd {
+ 	public:
+ 	CMainWin();
+ 	afx_msg void OnPaint();
+ 	// Обработчики команд меню и акселераторов
+ 	afx_msg void OnCommand_Alpha();
+ 	afx_msg void OnCommand_Beta();
+ 	afx_msg void OnCommand_Gamma();
+ 	afx_msg void OnCommand_Epsilon();
+ 	afx_msg void OnCommand_Zeta();
+ 	afx_msg void OnCommand_Eta();
+ 	afx_msg void OnCommand_Theta();
+ 	afx_msg void OnCommand_Help();
+ 	afx_msg void OnCommand_Time(); //Только акселератор
+ 	DECLARE_MESSAGE_MAP()
+ 	};
+
+	// Класс приложения
+	class CApp: public CWinApp {
+   	public:
+ 	BOOL InitInstance();
+ 	};
