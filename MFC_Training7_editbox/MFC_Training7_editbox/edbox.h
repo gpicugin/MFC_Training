65c6b83a5fbdc74f77fec984824160f0d99716ee
Unknown <gpicugin@gmail.com>
2018-11-29 12:36:54 +0300

    MFC_Training7_editbox end

---
+++ MFC_Training7_editbox/MFC_Training7_editbox/edbox.h
@@ -0,0 +1,47 @@
+#include <afxwin.h>
+#include "resource.h"
+
+// Класс главного окна
+class CMainWin: public CFrameWnd {
+public:
+	// Конструктор немного изменен.
+	// Title -- заголовок окна, HowShow -- код показа окна 	
+	CMainWin(CString Title, int HowShow); 
+	afx_msg void OnCommand_Dialog(); 
+	afx_msg void OnCommand_Exit();
+	afx_msg void OnCommand_Help();
+	afx_msg void OnPaint();	
+	afx_msg void OnDestroy();	
+	// Установка строки, отображаемой в верхней
+	// части окна
+	virtual void SetInfoStr(CString str);
+private:
+	CString infoStr; 
+	// Строка, отображаемая в верхней части окна
+	DECLARE_MESSAGE_MAP()
+};
+
+// Класс приложения
+class CApp: public CWinApp {
+public:
+	BOOL InitInstance();	
+};
+// Класс диалогового окна
+class CSampleDialog: public CDialog {
+public:
+	// DialogName -- идентификатор диалога в ресурсах,
+	// Owner -- окно-владелец (если NULL, то главное окно)
+	CSampleDialog(char *DialogName, CWnd *Owner = NULL);
+	BOOL OnInitDialog();
+	afx_msg void OnCommand_Red();
+	afx_msg void OnCommand_Green();
+	afx_msg void OnCommand_EditOK();
+
+	// Здесь читаются данные поля ввода
+	afx_msg void OnSelectFruit();
+	afx_msg void OnCancel();
+private:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnLbnDblclkList1();
+};
